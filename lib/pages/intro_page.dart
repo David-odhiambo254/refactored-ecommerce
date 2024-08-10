0e5d090f28ea342a551695069207c56130d2e0db
Dave <you@example.com>
2024-08-11 00:57:00 +0300

    intro page

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/components/my_button.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          //logo
+          Icon(
+            Icons.shopping_bag,
+            size: 72,
+          ),
+          const SizedBox(height: 20,),
+
+          //title
+          Text(
+            "Minimal Shop",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(height: 10,),
+
+          //subtitle
+          Text(
+            "Premium quality product",
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+          ),
+          
+          const SizedBox(height: 25), 
+
+          //button
+          MyButton(onTap: () {}, child: Icon(Icons.arrow_forward))
+        ],
+      ),
+      )
+    );
+  }
+}
